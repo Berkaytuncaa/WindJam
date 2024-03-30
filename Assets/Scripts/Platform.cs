e8621958942cc5a1499193db647cbe2920b85ed5
tunaeskanaytac <tunaeskanaytac@gmail.com>
2024-03-30 21:41:50 +0300

    Merge branch 'FireElemental' of https://github.com/Berkaytuncaa/WindJam into FireElemental

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -1,11 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using FireElemental;
 using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
     [SerializeField] private Vector2 openPosition;
     [SerializeField] private float moveSpeed;
+
+    private void Awake()
+    {
+        FireElementalController.InteractPressed.AddListener(MovePlatform);
+    }
 
     private void MovePlatform()
     {
