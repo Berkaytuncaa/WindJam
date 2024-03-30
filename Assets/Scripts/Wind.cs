06f540b1ee1a4873e2b8cb5b9a7848bdf5860440
Berkay Tunca <berkaytunca40@gmail.com>
2024-03-30 20:13:26 +0300

    add Wind.cs

--- Assets/Scripts/Wind.cs
+++ Assets/Scripts/Wind.cs
@@ -1,19 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FireElemental;
 
 public class Wind : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            FireElementalController.Death();
+        }
     }
 }
