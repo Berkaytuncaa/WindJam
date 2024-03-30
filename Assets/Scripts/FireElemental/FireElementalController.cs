9f34b6f7dfec0bfb1e0d1f0be15326395f5dfcfc
tunaeskanaytac <tunaeskanaytac@gmail.com>
2024-03-30 19:35:42 +0300

    solve conflicts

--- Assets/Scripts/FireElemental/FireElementalController.cs
+++ Assets/Scripts/FireElemental/FireElementalController.cs
@@ -8,11 +8,8 @@ namespace FireElemental
     {
         private FireElementalControls _controls;
         private Rigidbody2D _fireElRb;
-<<<<<<< HEAD
         [SerializeField] private float jumpPower;
-=======
 
->>>>>>> 62f76a519161ecd3b43e33dde2acc99bc4df6a6b
         private void OnEnable()
         {
             if (_controls == null)
@@ -20,31 +17,26 @@ namespace FireElemental
                 _controls = new FireElementalControls();
                 _controls.Gameplay.SetCallbacks(this);
             }
+
             _controls.Gameplay.Enable();
         }
 
-<<<<<<< HEAD
         private void Awake()
         {
             _fireElRb = GetComponent<Rigidbody2D>();
         }
 
+
         // Start is called before the first frame update
         void Start()
         {
-            
-=======
-        // Start is called before the first frame update
-        void Start()
-        {
-        
->>>>>>> 62f76a519161ecd3b43e33dde2acc99bc4df6a6b
+
         }
 
         // Update is called once per frame
         void Update()
         {
-            
+
         }
 
         public void OnMove(InputAction.CallbackContext context)
@@ -54,11 +46,7 @@ namespace FireElemental
 
         public void OnJump(InputAction.CallbackContext context)
         {
-<<<<<<< HEAD
             _fireElRb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-=======
-            throw new NotImplementedException();
->>>>>>> 62f76a519161ecd3b43e33dde2acc99bc4df6a6b
         }
     }
 }
