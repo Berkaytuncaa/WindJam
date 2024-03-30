314fbd3ff90eff0f547b32c2fae9f55fb7029e4e
tunaeskanaytac <tunaeskanaytac@gmail.com>
2024-03-30 15:29:09 +0300

    Implement some more fred

--- Assets/Scripts/Fred/FredController.cs
+++ Assets/Scripts/Fred/FredController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,7 +7,9 @@ namespace Fred
     public class FredController : MonoBehaviour, FredControls.IFredPlayingActions
     {
         private FredControls _controls;
-
+        private Rigidbody2D _fredRb;
+        [SerializeField] private float moveSpeed;
+        
         private void OnEnable()
         {
             if (_controls == null)
@@ -14,23 +17,36 @@ namespace Fred
                 _controls = new FredControls();
                 _controls.FredPlaying.SetCallbacks(this);
             }
+            _controls.FredPlaying.Enable();
         }
 
         // Start is called before the first frame update
-        void Start()
+        private void Awake()
         {
-        
+            _fredRb = GetComponent<Rigidbody2D>();
         }
 
         // Update is called once per frame
         void Update()
         {
-        
+            
         }
 
         public void OnMove(InputAction.CallbackContext context)
         {
-        
+            _fredRb.velocity = new Vector2(context.ReadValue<Vector2>().normalized.x * moveSpeed, 0);
+        }
+
+        public void OnFire(InputAction.CallbackContext context)
+        {
+            var position = _fredRb.position;
+            _fredRb.AddForceAtPosition(transform.TransformPoint(Vector2.left * moveSpeed), transform.TransformPoint(Vector2.right *.2f), ForceMode2D.Impulse);
+        }
+
+        private void OnDrawGizmos()
+        {
+            var position = transform.position;
+            Gizmos.DrawWireSphere(transform.TransformPoint(Vector2.right * .2f), .05f);
         }
     }
 }
