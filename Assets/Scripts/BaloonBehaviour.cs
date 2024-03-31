d62efa83fc50b7d2adef8336f9c3820fe16b7744
tunaeskanaytac <tunaeskanaytac@gmail.com>
2024-03-31 22:13:07 +0300

    a dirty fix

--- Assets/Scripts/BaloonBehaviour.cs
+++ Assets/Scripts/BaloonBehaviour.cs
@@ -13,14 +13,16 @@ public class BaloonBehaviour : MonoBehaviour
         _rb = GetComponent<Rigidbody2D>();
     }
 
+    private bool _reachedLimit = false;
     private void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("Block"))
         {
+            _reachedLimit = true;
             _rb.velocity = Vector2.zero;
             return;
         }
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !_reachedLimit)
         {
             _rb.velocity = Vector2.up * _speed;
         }
