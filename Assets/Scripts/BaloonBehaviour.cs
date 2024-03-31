acf5ed6c281fd4369dce2e3635a34a409f84e2ca
tunaeskanaytac <tunaeskanaytac@gmail.com>
2024-03-31 22:09:49 +0300

    add BalloonLimit

--- Assets/Scripts/BaloonBehaviour.cs
+++ Assets/Scripts/BaloonBehaviour.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,27 +7,22 @@ public class BaloonBehaviour : MonoBehaviour
 {
     private Rigidbody2D _rb;
     private float _speed = 0.5f;
-    private float _stopSpeed = 0f;
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D other)
     {
-        if (collision.CompareTag("Player"))
+        if (other.CompareTag("Block"))
+        {
+            _rb.velocity = Vector2.zero;
+            return;
+        }
+        if (other.CompareTag("Player"))
         {
             _rb.velocity = Vector2.up * _speed;
         }
     }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            _rb.velocity = Vector2.up * _stopSpeed;
-        }
-    }
-
 }
